c7ec43d78319d64b339e29b7b5306ef6fedae4ee
Pochang Lee <webmaster@classfigure.com>
2017-01-23 13:34:55 +0800

    config

--- server/server/settings.py
+++ server/server/settings.py
@@ -121,5 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    ("", os.path.join(BASE_DIR, "../client/build/page/dist")),
+    ("", os.path.join(BASE_DIR, "../client/dist")),
 )
